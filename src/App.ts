7ef18646f2d156d599ac040f7ee47a9168218df4
maxdevback <zacharchuk.max.dev@gmail.com>
2023-09-26 15:50:01 +0300

    Added express

--- src/App.ts
+++ src/App.ts
@@ -1,1 +1,17 @@
-console.log("Hello world!!!")
+import express from "express"
+
+const App = express()
+
+
+App.all("*", (req, res) => {
+  res.send("Hello world")
+})
+
+const start = async () => {
+  const port = 3000
+  App.listen(port,() => {
+    console.log(`App has been started at ${port} port`)
+  })
+}
+
+start()
